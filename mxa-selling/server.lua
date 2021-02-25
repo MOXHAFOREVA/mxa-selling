510fd0e38237bf17cf97d49ffca8de72ed97fd98
MOXHAFOREVA <70913098+MOXHAFOREVA@users.noreply.github.com>
2021-02-25 12:52:57 +0300

    Update server.lua

--- mxa-selling/server.lua
+++ mxa-selling/server.lua
@@ -5,14 +5,28 @@ RegisterServerEvent('mxa-selling:sell')
 AddEventHandler('mxa-selling:sell', function (item, price)
     local src = source
     local xPlayer = ESX.GetPlayerFromId(src)
-    local xItem = xPlayer.getInventoryItem(item)
         if item ~= nil or price ~= nil then
-        if xItem.count >= 1 then
-            xPlayer.addMoney(price * xItem.count)
-            xPlayer.removeInventoryItem(item, xItem.count)
-            xPlayer.showNotification('Şu eşyayı '..item..' şukadara '..price*xItem.count..' sattın ')
-        else
-            xPlayer.showNotification('Üstünüzde şu eşyadan '..item..' yeterli sayıda yok')
-        end
+            if type(item) == "table" then
+                for _, items in pairs(item) do
+                      local xItem = xPlayer.getInventoryItem(items)
+                    if xItem.count >= 1 then
+                        xPlayer.addMoney(price * xItem.count)
+                        xPlayer.removeInventoryItem(item, xItem.count)
+                        xPlayer.showNotification('Şu eşyayı '..item..' şukadara '..price*xItem.count..' sattın ')
+                    else
+                        xPlayer.showNotification('Üstünüzde şu eşyadan '..items..' yeterli sayıda yok')
+                    end
+                end
+                
+                else
+                   local xItem = xPlayer.getInventoryItem(item)
+                if xItem.count >= 1 then
+                xPlayer.addMoney(price * xItem.count)
+                xPlayer.removeInventoryItem(item, xItem.count)
+                xPlayer.showNotification('Şu eşyayı '..item..' şukadara '..price*xItem.count..' sattın ')
+            else
+                xPlayer.showNotification('Üstünüzde şu eşyadan '..item..' yeterli sayıda yok')
+            end
+       end
     end
 end)
